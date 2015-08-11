0848236d9d63c85ee89252531fa408f5167cdc3c
s-nakajima <nakajimashouhei@gmail.com>
2015-08-11 12:16:10 +0900

    ルーム管理の開発によるプラグインデータの修正

--- Config/Migration/001_plugin_records.php
+++ Config/Migration/001_plugin_records.php
@@ -69,28 +69,12 @@ class PluginRecords extends NetCommonsMigration {
 			),
 		),
 		'PluginsRoom' => array(
-			array(
-				'room_id' => '1',
-				'plugin_key' => 'links'
-			),
-		),
-		'PluginsSpace' => array(
-			array(
-				'space_id' => '1',
-				'plugin_key' => 'links'
-			),
-			array(
-				'space_id' => '2',
-				'plugin_key' => 'links',
-			),
-			array(
-				'space_id' => '3',
-				'plugin_key' => 'links',
-			),
-			array(
-				'space_id' => '4',
-				'plugin_key' => 'links',
-			),
+			//パブリックスペース
+			array('room_id' => '1', 'plugin_key' => 'links', ),
+			//プライベートスペース
+			array('room_id' => '2', 'plugin_key' => 'links', ),
+			//グループスペース
+			array('room_id' => '3', 'plugin_key' => 'links', ),
 		),
 	);
 
