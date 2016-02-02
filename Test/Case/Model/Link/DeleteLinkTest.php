ec98ada945841c00f72db4522b3a66b46391845a
s-nakajima <nakajimashouhei@gmail.com>
2016-02-02 16:26:29 +0900

    Link::deleteLink()のPHPUnitコミット

---
+++ Test/Case/Model/Link/DeleteLinkTest.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Link::deleteLink()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('WorkflowDeleteTest', 'Workflow.TestSuite');
+App::uses('LinkFixture', 'Links.Test/Fixture');
+App::uses('LinkOrderFixture', 'Links.Test/Fixture');
+
+/**
+ * Link::deleteLink()のテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Links\Test\Case\Model\Link
+ */
+class LinkDeleteLinkTest extends WorkflowDeleteTest {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.categories.category',
+		'plugin.categories.category_order',
+		'plugin.links.link',
+		'plugin.links.link_frame_setting',
+		'plugin.links.link_order',
+		'plugin.links.link_setting',
+		'plugin.workflow.workflow_comment',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'links';
+
+/**
+ * Model name
+ *
+ * @var string
+ */
+	protected $_modelName = 'Link';
+
+/**
+ * Method name
+ *
+ * @var string
+ */
+	protected $_methodName = 'deleteLink';
+
+/**
+ * Delete用DataProvider
+ *
+ * ### 戻り値
+ *  - data: 削除データ
+ *  - associationModels: 削除確認の関連モデル array(model => conditions)
+ *
+ * @return array テストデータ
+ */
+	public function dataProviderDelete() {
+		$data['Link'] = (new LinkFixture())->records[0];
+		$data['LinkOrder'] = (new LinkOrderFixture())->records[0];
+
+		$association = array();
+
+		$results = array();
+		$results[0] = array($data, $association);
+
+		return $results;
+	}
+
+/**
+ * ExceptionError用DataProvider
+ *
+ * ### 戻り値
+ *  - data 登録データ
+ *  - mockModel Mockのモデル
+ *  - mockMethod Mockのメソッド
+ *
+ * @return array テストデータ
+ */
+	public function dataProviderDeleteOnExceptionError() {
+		$data['Link'] = (new LinkFixture())->records[0];
+		$data['LinkOrder'] = (new LinkOrderFixture())->records[0];
+
+		return array(
+			array($data, 'Links.Link', 'deleteAll'),
+			array($data, 'Links.LinkOrder', 'delete'),
+		);
+	}
+
+}
