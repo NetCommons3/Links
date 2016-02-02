67adf2ec4d58be64273b2f172099b3192fa0a0a1
s-nakajima <nakajimashouhei@gmail.com>
2016-02-02 17:03:08 +0900

    View/Elements/LinkBlocks/delete_formのPHPUnitテスト

---
+++ Test/Case/View/Elements/LinkBlocks/DeleteFormTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * View/Elements/LinkBlocks/delete_formのテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+
+/**
+ * View/Elements/LinkBlocks/delete_formのテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Links\Test\Case\View\Elements\LinkBlocks\DeleteForm
+ */
+class ViewElementsLinkBlocksDeleteFormTest extends NetCommonsControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array();
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'links';
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		//テストプラグインのロード
+		NetCommonsCakeTestCase::loadTestPlugin($this, 'Links', 'TestLinks');
+	}
+
+/**
+ * View/Elements/LinkBlocks/delete_formのテスト
+ *
+ * @return void
+ */
+	public function testDeleteForm() {
+		//テストコントローラ生成
+		$this->generateNc('TestLinks.TestViewElementsLinkBlocks');
+
+		//テスト実行
+		$this->_testNcAction('/test_links/test_view_elements_link_blocks/delete_form', array(
+			'method' => 'get'
+		));
+
+		//チェック
+		$pattern = '/' . preg_quote('View/Elements/LinkBlocks/delete_form', '/') . '/';
+		$this->assertRegExp($pattern, $this->view);
+
+		$this->assertInput('input', 'data[Block][id]', '1', $this->view);
+		$this->assertInput('input', 'data[Block][key]', 'block_key_1', $this->view);
+		$this->assertInput('input', 'data[LinkBlock][key]', 'block_key_1', $this->view);
+
+		$this->assertInput('button', 'delete', null, $this->view);
+	}
+
+}
