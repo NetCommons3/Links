871216ed07d214b5e609829008f7a8eb049dd9fe
s-nakajima <nakajimashouhei@gmail.com>
2016-02-15 11:50:06 +0900

    LinkOrder::validate()のテスト

---
+++ Test/Case/Model/LinkOrder/ValidateTest.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * LinkOrder::validate()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsValidateTest', 'NetCommons.TestSuite');
+App::uses('LinkOrderFixture', 'Links.Test/Fixture');
+
+/**
+ * LinkOrder::validate()のテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Links\Test\Case\Model\LinkOrder
+ */
+class LinkOrderValidateTest extends NetCommonsValidateTest {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.categories.category',
+		'plugin.categories.category_order',
+		'plugin.links.link',
+		'plugin.links.link_frame_setting',
+		'plugin.links.link_order',
+		'plugin.links.link_setting',
+		'plugin.workflow.workflow_comment',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'links';
+
+/**
+ * Model name
+ *
+ * @var string
+ */
+	protected $_modelName = 'LinkOrder';
+
+/**
+ * Method name
+ *
+ * @var string
+ */
+	protected $_methodName = 'validate';
+
+/**
+ * ValidationErrorのDataProvider
+ *
+ * ### 戻り値
+ *  - data 登録データ
+ *  - field フィールド名
+ *  - value セットする値
+ *  - message エラーメッセージ
+ *  - overwrite 上書きするデータ(省略可)
+ *
+ * @return array テストデータ
+ */
+	public function dataProviderValidationError() {
+		$data['LinkOrder'] = (new LinkOrderFixture())->records[0];
+
+		return array(
+			array('data' => $data, 'field' => 'block_key', 'value' => '',
+				'message' => __d('net_commons', 'Invalid request.')),
+			array('data' => $data, 'field' => 'link_key', 'value' => '',
+				'message' => __d('net_commons', 'Invalid request.')),
+			array('data' => $data, 'field' => 'weight', 'value' => 'a',
+				'message' => __d('net_commons', 'Invalid request.')),
+		);
+	}
+
+}
