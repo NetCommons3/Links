c307d06a8298ac6656ebdc63c411a7871e3e2ad0
s-nakajima <nakajimashouhei@gmail.com>
2016-02-15 10:39:03 +0900

    LinkBlocksController::index()のテスト

---
+++ Test/Case/Controller/LinkBlocksController/IndexTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * LinkBlocksController::index()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('BlocksControllerTest', 'Blocks.TestSuite');
+
+/**
+ * LinkBlocksController::index()のテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Links\Test\Case\Controller\LinkBlocksController
+ */
+class LinkBlocksControllerIndexTest extends BlocksControllerTest {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.categories.category',
+		'plugin.categories.category_order',
+		'plugin.links.link',
+		'plugin.links.link_frame_setting',
+		'plugin.links.link_order',
+		'plugin.links.link_setting',
+		'plugin.workflow.workflow_comment',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'links';
+
+/**
+ * Controller name
+ *
+ * @var string
+ */
+	protected $_controller = 'link_blocks';
+
+/**
+ * Edit controller name
+ *
+ * @var string
+ */
+	protected $_editController = 'link_blocks';
+
+}
