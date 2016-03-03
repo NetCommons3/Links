3659aba4a35d50dea936df298b8c6cb4b440a1b8
s-nakajima <nakajimashouhei@gmail.com>
2016-03-03 16:06:51 +0900

    LinkFrameSetting::saveLinkFrameSetting()のテスト

---
+++ Test/Case/Model/LinkFrameSetting/SaveLinkFrameSettingTest.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * LinkFrameSetting::saveLinkFrameSetting()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsSaveTest', 'NetCommons.TestSuite');
+App::uses('LinkFrameSettingFixture', 'Links.Test/Fixture');
+
+/**
+ * LinkFrameSetting::saveLinkFrameSetting()のテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Links\Test\Case\Model\LinkFrameSetting
+ */
+class LinkFrameSettingSaveLinkFrameSettingTest extends NetCommonsSaveTest {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.categories.category',
+		'plugin.categories.category_order',
+		'plugin.links.link',
+		'plugin.links.link_frame_setting',
+		'plugin.links.link_order',
+		'plugin.links.link_setting',
+		'plugin.workflow.workflow_comment',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'links';
+
+/**
+ * Model name
+ *
+ * @var string
+ */
+	protected $_modelName = 'LinkFrameSetting';
+
+/**
+ * Method name
+ *
+ * @var string
+ */
+	protected $_methodName = 'saveLinkFrameSetting';
+
+/**
+ * Save用DataProvider
+ *
+ * ### 戻り値
+ *  - data 登録データ
+ *
+ * @return array テストデータ
+ */
+	public function dataProviderSave() {
+		$data['LinkFrameSetting'] = Hash::merge((new LinkFrameSettingFixture())->records[0], array(
+			'display_type' => '1',
+			'open_new_tab' => false,
+			'display_click_count' => false,
+			'category_separator_line' => 'line_a3.gif',
+			'list_style' => 'mark_a3.gif',
+		));
+
+		$results = array();
+		// * 編集の登録処理
+		$results[0] = array($data);
+		// * 新規の登録処理
+		$results[1] = array($data);
+		$results[1] = Hash::insert($results[1], '0.LinkFrameSetting.id', null);
+		$results[1] = Hash::remove($results[1], '0.LinkFrameSetting.created_user');
+
+		return $results;
+	}
+
+/**
+ * SaveのExceptionError用DataProvider
+ *
+ * ### 戻り値
+ *  - data 登録データ
+ *  - mockModel Mockのモデル
+ *  - mockMethod Mockのメソッド
+ *
+ * @return array テストデータ
+ */
+	public function dataProviderSaveOnExceptionError() {
+		$data = $this->dataProviderSave()[0][0];
+
+		return array(
+			array($data, 'Links.LinkFrameSetting', 'save'),
+		);
+	}
+
+/**
+ * SaveのValidationError用DataProvider
+ *
+ * ### 戻り値
+ *  - data 登録データ
+ *  - mockModel Mockのモデル
+ *  - mockMethod Mockのメソッド(省略可：デフォルト validates)
+ *
+ * @return array テストデータ
+ */
+	public function dataProviderSaveOnValidationError() {
+		$data = $this->dataProviderSave()[0][0];
+
+		return array(
+			array($data, 'Links.LinkFrameSetting'),
+		);
+	}
+
+}
