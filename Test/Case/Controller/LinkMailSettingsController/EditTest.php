5377257ca8468a80517a94024ad0bf0c4290d8d8
s-nakajima <nakajimashouhei@gmail.com>
2016-04-02 17:51:03 +0900

    メールのUnitテスト追加

---
+++ Test/Case/Controller/LinkMailSettingsController/EditTest.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * LinkMailSettingsController::edit()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+
+/**
+ * LinkMailSettingsController::edit()のテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Links\Test\Case\Controller\LinkMailSettingsController
+ */
+class LinkMailSettingsControllerEditTest extends NetCommonsControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.categories.category',
+		'plugin.categories.category_order',
+		'plugin.links.link',
+		'plugin.links.link_frame_setting',
+		'plugin.links.link_order',
+		'plugin.links.link_setting',
+		'plugin.workflow.workflow_comment',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'links';
+
+/**
+ * Controller name
+ *
+ * @var string
+ */
+	protected $_controller = 'link_mail_settings';
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		//ログイン
+		TestAuthGeneral::login($this);
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		//ログアウト
+		TestAuthGeneral::logout($this);
+
+		parent::tearDown();
+	}
+
+/**
+ * edit()アクションのGetリクエストテスト
+ *
+ * @return void
+ */
+	public function testEditGet() {
+		//テストデータ
+		$frameId = '6';
+		$blockId = '2';
+
+		//テスト実行
+		$this->_testGetAction(array('action' => 'edit', 'block_id' => $blockId, 'frame_id' => $frameId),
+				array('method' => 'assertNotEmpty'), null, 'view');
+
+		//チェック
+		$this->assertInput('form', null, 'links/link_mail_settings/edit/' . $blockId, $this->view);
+	}
+
+}
